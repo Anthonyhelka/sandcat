b90cb0d6b234e192d100dd1732d089a83f6dcbcc
Anthonyhelka <helkaanthony1997@gmail.com>
2020-03-11 20:35:18 -0400

    Updated README.md. Added -help option to CLI.

--- bin/index.js
+++ bin/index.js
@@ -1,5 +1,16 @@
 #!/usr/bin/env node
 
-const sandcat = require('../lib/index.js');
+const colors = require('colors');
 
-sandcat(process.argv[2]);
+const sandcat = require('../lib/index.js');
+const rootJSON = require('../package.json');
+
+const helpMessage = `Need some help? Here is a list of Sandcat options:\n\nsandcat <application_name>   create MERN stack application\nsandcat -help                see list of Sandcat options\n\nsandcat@${rootJSON.version} | https://www.npmjs.com/package/sandcat`;
+
+if (process.argv[2] === '-help' || process.argv[2] === 'help') {
+  console.log(helpMessage.bold.cyan);
+} else if (String(process.argv[2])) {
+  sandcat(process.argv[2]);
+} else {
+  console.log(helpMessage.bold.cyan);
+}
